99c5d8ec4495952964df9095779c8529290cab92
tpwiedy <wiedy.pratama@gmail.com>
2022-06-04 09:54:19 +0700

    feat: challenge 2 Done

--- index.js
+++ index.js
@@ -21,10 +21,35 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (dataBelanjaan) => {
+  let data = [];
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    // looping data
+    let loop = "";
+    loop += "- " + dataBelanjaan[i].nama + " x " + dataBelanjaan[i].kuantitas;
+    // masukan ke array
+    data.push(loop);
+  }
+  return data;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (dataBelanjaan) => {
+  let data = [];
+  let sum = 0;
+  // array dari summary
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    // looping data
+    let loop = "";
+    loop += dataBelanjaan[i].harga * dataBelanjaan[i].kuantitas;
+    // masukan ke array
+    data.push(Number(loop));
+  }
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+  }
+  return sum;
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
